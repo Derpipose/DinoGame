c2b7cf8d3b36ac00cf253291353f705d7c8bcbde
Derpipose <thefluffmaster2000@gmail.com>
2022-10-28 11:24:33 -0600

    Charaters created Board built

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using DinoGame;
+
+Thread.Sleep(50);
+Board game = new Board();
+for(int i = 0; i < 7; i++)
+{
+    for(int j = 0; j < 10; j++)
+    {
+        Console.Write(game.board[i, j]);
+    }
+    Console.Write("\n");
+}
--- DinoGame/Class1.cs
+++ DinoGame/Class1.cs
@@ -1,4 +1,6 @@
-﻿namespace DinoGame
+﻿using System.Globalization;
+
+namespace DinoGame
 {
     public class DinoClass
     {
@@ -7,11 +9,47 @@
 
     public class Board
     {
+        DinoRunner texture = new DinoRunner();
+        int j = 0;
+        bool gameRunning = true;
+        public char[,] board = new char[10, 7];
+        //public char[] runner = new char[2] { texture.Dino1[0], texture.Dino2[0] };   
+        public void gameState()
+        {
+            do
+            {
+                for(int i = 0; i < 10; i++)
+                {
+                    board[i, 1] = texture.Ground[(i+j)%3];
+                    if(i%2 == 0)
+                    {
+                        board[2, 2] = texture.Dino1[0];
+                        board[2, 3] = texture.Dino1[1];
+                        board[2, 4] = texture.Dino1[2];
+                        board[3, 2] = '\u0032';
+                    }
+                    else
+                    {
+                        board[2, 2] = texture.Dino2[0];
+                        board[3, 2] = texture.Dino2[3];
+                        board[2, 3] = texture.Dino2[1];
+                        board[2, 4] = texture.Dino2[2];
+                    }
+                }
+                j++;
 
+            } while (gameRunning);
+        }
     }
 
     public class DinoRunner
     {
+        public char[] Ground = {'\u0176', '\u0178', '\u0177'};
+        public char[] Dino1 = { '\u0202', '\u0225', '\u0149' };
+        public char[] Dino2 = { '\u0217', '\u0225', '\u0149', '\u0192'};
+        public char[] Cactus1 = { '\u0241' };
+        public char[] Cactus2 = { '\u0240', '\u0241' };
+        public char[] Bird = { '\u0174', '\u0170' };
 
     }
 }
