9190fa8db5c3d47c24ff16426b9b209cb7c72d91
Derpipose <thefluffmaster2000@gmail.com>
2022-11-01 14:41:10 -0600

    Implemented mover interface

---
+++ DinoGame/MoverInterface.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinoGame
+{
+    internal interface Mover
+    {
+        static Board screen = new Board();
+        int x { get; set; }
+        int y { get; set; }
+        char image { get; set; }
+        public void MoveForward()
+        {
+            screen.board[x, y] = '\u0032';
+            screen.board[x-=1,y] = image;
+        }
+        public bool HitPlayerCheck()
+        {
+            if(screen.board[x, y] != image)
+            {
+                return true;
+            }
+            else return false;  
+        }
+    }
+}
