2c966d58b37b5939447785fccc9cbe41d52cd8c4
Derpipose <thefluffmaster2000@gmail.com>
2022-11-07 11:10:54 -0700

    Got jump and duck working

--- DinoGame/Player.cs
+++ DinoGame/Player.cs
@@ -36,16 +36,73 @@ namespace DinoGame
 
         public void MoveForward(int status)
         {
+            if(status >= 0 && playerLocation.x.Count == 2)
+            {
+                playerLocation.x.Add(2);
+                playerLocation.y.Add(4);
+            }
             if (status > 0)
             {
                 //ducking
+                if (status == 2)
+                {
+                    playerStanding = true;
+
+                    if (playerLocation.x.Count == 4)
+                    {
+                        playerLocation.x.RemoveAt(3);
+                        playerLocation.y.RemoveAt(3);
+                    }
+                    playerLocation.x.RemoveAt(2);
+                    playerLocation.y.RemoveAt(2);
+
+                }
                 playerDucking = true;
                 playerJump = status;
+
 
             }
             else if (status < 0)
             {
                 //jumping
+                if(status == -4 )
+                {
+                    playerStanding = true;
+                    
+                    playerLocation.y[0]=playerLocation.y[0]+1;
+                    playerLocation.y[1] = playerLocation.y[1] + 1;
+                    playerLocation.y[2] = playerLocation.y[2] + 1;
+                    if(playerLocation.x.Count == 4)
+                    {
+                        playerLocation.x.RemoveAt(3);
+                        playerLocation.y.RemoveAt(3);
+                    }
+
+
+                }
+                else if(status == -3)
+                {
+
+                    playerLocation.y[0] = playerLocation.y[0] + 1;
+                    playerLocation.y[1] = playerLocation.y[1] + 1;
+                    playerLocation.y[2] = playerLocation.y[2] + 1;
+                    
+                }
+                else if (status == -2)
+                {
+                    playerLocation.y[0] = playerLocation.y[0] - 1;
+                    playerLocation.y[1] = playerLocation.y[1] - 1;
+                    playerLocation.y[2] = playerLocation.y[2] - 1;
+
+                }
+                else if (status == -1)
+                {
+                    playerLocation.y[0] = playerLocation.y[0] - 1;
+                    playerLocation.y[1] = playerLocation.y[1] - 1;
+                    playerLocation.y[2] = playerLocation.y[2] - 1;
+
+                }
+
                 playerJumping = true;
                 playerJump = status;
 
@@ -78,26 +135,10 @@ namespace DinoGame
         public void Print(char[,] board)
         {
             if (playerDucking == true)
-            {
+            {   
                 board[playerLocation.x[0], playerLocation.y[0]] = stand[0];
                 board[playerLocation.x[1], playerLocation.y[1]] = stand[2];
                 return;
-            }
-            else if(playerJumping == true)
-            {
-                if (playerJump == 4 || playerJump == 1)
-                {
-                    board[playerLocation.x[0], playerLocation.y[0] + 1] = stand[0];
-                    board[playerLocation.x[1], playerLocation.y[1] + 1] = stand[1];
-                    board[playerLocation.x[2], playerLocation.y[2] + 1] = stand[2];
-                }
-                else
-                {
-                    board[playerLocation.x[0], playerLocation.y[0] + 2] = stand[0];
-                    board[playerLocation.x[1], playerLocation.y[1] + 2] = stand[1];
-                    board[playerLocation.x[2], playerLocation.y[2] + 2] = stand[2];
-                }
-
             }
             else if (playerStanding == true)
             {
